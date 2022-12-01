a5304970d876360b6780dfa4d4d290eaec65eb68
Jordan Cleigh <cleigh.jordan@gmail.com>
2022-12-01 11:46:38 -0600

    Perma-blame Fonzi

--- lib/CowBlame.rb
+++ lib/CowBlame.rb
@@ -1,11 +1,11 @@
 class CowBlame
   def self.cow
-    name = File.join(File.dirname(__FILE__), '/names.txt')
-    random_line = File.readlines(name).sample
-    random_line.gsub!(/\r\n?/, "")
+    #name = File.join(File.dirname(__FILE__), '/names.txt')
+    #random_line = File.readlines(name).sample
+    #random_line.gsub!(/\r\n?/, "")
 
     cow = "
-    IT WAS #{random_line.upcase.chomp}
+    IT WAS FONZI
     I SWEAR!
     THAT PERSON BROKE IT!
           \\   ^__^
